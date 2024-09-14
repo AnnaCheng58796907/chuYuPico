c9364911c5bdcae34c8b186a23f1d8c8b0b6b0b0
鄭筑予 <q5j6jp6m3@gmail.com>
2024-09-14 14:18:26 +0800

    lesson5

--- lesson5/lesson5_2_3.py
+++ lesson5/lesson5_2_3.py
@@ -25,28 +25,38 @@ def input_data():
             continue
     return cm, kg
 
+#(bmi:float)->意思是提供float給這function,這function會傳出字串
+def get_status(bmi:float)->str:
+    status = bmi
+    if bmi >=35:
+        return "重度肥胖:BMI≧ 35"
+    elif bmi >=30:
+        return "中度肥胖:30≦ BMI"
+    elif bmi >=27:
+        return "輕度肥胖:27≦ BMI"
+    elif bmi >=24:
+        return "過重"
+    elif bmi >=18.5:
+        return "正常範圍"
+    else:
+     return "體重過輕"
 
+def calculate_bmi(cm:int, kg:int)->float:
+    cm=(cm/100)**2
+    BMI=kg/cm
+    return BMI
+
+#主程式
 while True:
     kg=0  #清除變數
     cm=0  #清除變數
     cm, kg = input_data() #呼叫function
 
     print(f'身高={cm},體重={kg}')
-    cm=(cm/100)*(cm/100)
-    BMI=kg/cm
+    BMI = calculate_bmi(cm=cm, kg=kg )#引數名稱的呼叫,可以不依照順序
     print(f'BMI={BMI}')
-    if BMI >=35:
-        print("重度肥胖：BMI≧35")
-    elif BMI >=30:
-        print("中度肥胖：30≦BMI")
-    elif BMI >=27:
-        print("輕度肥胖：27≦BMI")
-    elif BMI >=24:
-        print("過重")
-    elif BMI >=18.5:
-        print("正常範圍")
-    else:
-        print("體重過輕")
+    print(get_status(BMI))
+    
     
     repeat = input("你要繼續嗎(y/n):")
     if  repeat == 'n' or repeat == 'N' :
