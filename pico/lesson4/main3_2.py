33491af82b747043fdac823fbb567909ef1aa6e4
鄭筑予 <q5j6jp6m3@gmail.com>
2024-09-21 16:08:51 +0800

    lesson4_pico

--- pico/lesson4/main3_2.py
+++ pico/lesson4/main3_2.py
@@ -6,18 +6,19 @@ green_count = 0
 def green_led_mycallback(t:Timer):
     global green_count
     green_count += 1
-    #green_led.toggle()
+    green_led.toggle()
     print(f'green_led執行第{green_count}次')
     if green_count >=10 :
         t.deinit()
         
 green_led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=green_led_mycallback)
 
+red_led = Pin(15, Pin.OUT)
 red_count = 0
 def red_led_mycallback(t:Timer):
     global red_count
     red_count += 1
-    #red_led.toggle()
+    red_led.toggle()
     print(f'red_led執行第{red_count}次')
     if red_count >=10 :
         t.deinit()
