1243664126b140f5b44ff59ea215f6781877d1e3
鄭筑予 <q5j6jp6m3@gmail.com>
2024-09-28 14:13:04 +0800

    lesson6_7_pico

--- pico/lesson6_7/main2_3.py
+++ pico/lesson6_7/main2_3.py
@@ -1,6 +1,7 @@
-from machine import ADC, Timer, Pin
+from machine import ADC, Timer, Pin, PWM
 
 adc = ADC(4)
+pwm = PWM(Pin(15),freq=50)
 conversion_factor = 3.3/(65535)
 
 def do_thing(t):
@@ -11,7 +12,10 @@ def do_thing(t):
 def do_thing1(t):
     adc1 = ADC(Pin(26))
     duty = adc1.read_u16()
+    
+    pwm.duty_u16(duty)
     print(f"可變電阻{round(duty/65535*10)}")
 
 tim1 = Timer(period=2000, callback=do_thing)
 tim2 = Timer(period=500,callback=do_thing1)
+
--- pico/lesson6_7/main3.py
+++ pico/lesson6_7/main3.py
@@ -1,17 +1,25 @@
-from machine import ADC, Timer, Pin
+from machine import ADC, Timer, Pin, PWM, RTC
 
 adc = ADC(4)
+pwm = PWM(Pin(15),freq=50)
 conversion_factor = 3.3/(65535)
+rtc = RTC()
 
 def do_thing(t):
     temperature_value = adc.read_u16()
     temperature = 27 - ((temperature_value*conversion_factor)-0.706)/0.001721
+    year, month,day, weekly, hours, minutes, seconds, info = rtc.datetime()
+    datetime_str = f"{year}-{month}-{day} {hours}:{minutes}:{seconds}"
+    print(datetime_str)
     print(temperature)
 
 def do_thing1(t):
     adc1 = ADC(Pin(26))
     duty = adc1.read_u16()
+    pwm.duty_u16(duty)
     print(f"可變電阻{round(duty/65535*10)}")
 
 tim1 = Timer(period=2000, callback=do_thing)
 tim2 = Timer(period=500,callback=do_thing1)
+
+
