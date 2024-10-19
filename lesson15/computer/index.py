6cff774b7c5be0583403966d4569c111b9083115
鄭筑予 <q5j6jp6m3@gmail.com>
2024-10-19 14:15:06 +0800

    lesson15_3

--- lesson15/computer/index.py
+++ lesson15/computer/index.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os, csv
 
 #def record(r:list[str, str, int]):
-def record(date:str, topic:str, value:int):
+def record(date:str, topic:str, value:int | float):
     '''
     # 檢查是否有data資料夾,沒有就建立data資料夾
     # 取得今天日期,並建立今天日期的csv檔
@@ -41,18 +41,28 @@ def on_connect(client, userdata, flags, reason_code, properties):
 
 def on_message(client, userdata, msg):
     global led_origin_value#變更為全域變數
+    global temperature_origin_value
     topic = msg.topic
     value = msg.payload.decode()
+    #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+    today = datetime.now()
+    now_str = today.strftime('%Y-%m-%d %H:%M:%S')
     if topic =='SA-56/LED_LEVEL':
         led_value = int(value)
         if led_value != led_origin_value:
             led_origin_value = led_value
             print(f'led_value:{led_value}')
-            today = datetime.now()
-            now_str = today.strftime('%Y-%m-%d %H:%M:%S')
+            
             #save_data = [now_str, 'SA-56/LED_LEVEL', led_value]
             record(now_str, topic, led_value)
-    #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+
+    if topic == 'SA-56/TEMPERATURE':
+        temperature_value = float(value)
+        if temperature_value != temperature_origin_value:
+            temperature_value = value
+            print(f'溫度:{value}')
+            record(now_str, topic, temperature_value)
+    
 
 def main():
     client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
@@ -69,4 +79,5 @@ def main():
 if __name__ == "__main__":
 
     led_origin_value = 0
+    temperature_origin_value = 0.0
     main()
